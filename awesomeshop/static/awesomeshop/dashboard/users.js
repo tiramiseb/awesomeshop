433cd29bf4cf76139e1a7ed0c03c13726e1b06c9
Sébastien Maccagnoni-Munch <seb+git@maccagnoni.eu>
2015-12-29 08:04:21 +0100

    Shorter names for submodules

--- awesomeshop/static/awesomeshop/dashboard/users.js
+++ awesomeshop/static/awesomeshop/dashboard/users.js
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with AwesomeShop. If not, see <http://www.gnu.org/licenses/>.
  */
-angular.module('dashboardUsers', [])
+angular.module('dbUsers', [])
 .config(function($stateProvider) {
     $stateProvider
         .state('users', {
