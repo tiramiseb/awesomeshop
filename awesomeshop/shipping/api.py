99122b19323e18e8d92be736f0ba5f4b2e823094
Sébastien Maccagnoni-Munch <seb+git@maccagnoni.eu>
2016-01-04 14:47:39 +0100

    100% AngularJS countries groups

--- awesomeshop/shipping/api.py
+++ awesomeshop/shipping/api.py
@@ -20,14 +20,15 @@
 from flask import abort, request
 from flask_babel import _
 from flask_restful import Resource
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
 from mongoengine import OperationError
 
 from .. import admin_required, login_required, rest
-from .models import Country
+from ..marsh import Count, MultiObjField
+from .models import Country, CountriesGroup
 
 class CountrySchemaForList(Schema):
-    id = fields.String()
+    id = fields.String(dump_only=True)
     code = fields.String(dump_only=True)
     name = fields.String(attribute='prefixed_name', dump_only=True)
 
@@ -36,6 +37,37 @@ class CountrySchema(Schema):
     code = fields.String(required=True)
     default_name = fields.String(required=True)
     name = fields.Dict(default={})
+
+    @post_load
+    def make_country(self, data):
+        if 'id' in data:
+            country = Country.objects.get_or_404(id=data['id'])
+        else:
+            country = Country()
+        country.code = data['code']
+        country.default_name = data['default_name']
+        country.name = data.get('name', {})
+        country.save()
+
+class CountriesGroupSchemaForList(Schema):
+    id = fields.String(dump_only=True)
+    name = fields.String(dump_only=True)
+    countries = Count()
+
+class CountriesGroupSchema(Schema):
+    id = fields.String()
+    name = fields.String(required=True)
+    countries = MultiObjField(f='code', obj=Country)
+
+    @post_load
+    def make_countriesgroup(self, data):
+        if 'id' in data:
+            group = CountriesGroup.objects.get_or_404(id=data['id'])
+        else:
+            group = CountriesGroup()
+        group.name = data['name']
+        group.countries = data['countries']
+        group.save()
 
 class ApiCountry(Resource):
     @login_required
@@ -65,5 +97,34 @@ class ApiCountry(Resource):
             raise
         return { 'status': 'OK' }
 
-rest.add_resource(ApiCountry, '/api/country', '/api/country/<country_id>')
+class ApiCountriesGroup(Resource):
+    @admin_required
+    def get(self, group_id=None):
+        if group_id:
+            return CountriesGroupSchema().dump(
+                           CountriesGroup.objects.get_or_404(id=group_id)).data
+        else:
+            return CountriesGroupSchemaForList(many=True).dump(
+                                                   CountriesGroup.objects).data
+    @admin_required
+    def post(self, group_id=None):
+        schema = CountriesGroupSchema()
+        data = request.get_json()
+        if group_id:
+            data['id'] = group_id
+        result, errors = schema.load(data)
+        return schema.dump(result).data
 
+    @admin_required
+    def delete(self, country_id):
+        try:
+            CountriesGroup.objects.get_or_404(id=country_id).delete()
+        except OperationError as e:
+            #if e.message == 'Could not delete document (CountriesGroup.countries refers to it)':
+            #    abort(400, { 'type': 'message', 'message': _('Could not delete : this country is part of a countries group.')})
+            raise
+        return { 'status': 'OK' }
+
+rest.add_resource(ApiCountry, '/api/country', '/api/country/<country_id>')
+rest.add_resource(ApiCountriesGroup, '/api/countriesgroup',
+                                     '/api/countriesgroup/<group_id>')
--- awesomeshop/static/awesomeshop/dashboard/countries.js
+++ awesomeshop/static/awesomeshop/dashboard/countries.js
@@ -34,6 +34,21 @@ angular.module('dbCountries', [])
             templateUrl: 'country',
             controller: 'CountryCtrl'
         })
+        .state('countriesgroups', {
+            url: '/contriesgroup',
+            templateUrl: 'countriesgroups',
+            controller: 'CountriesGroupsCtrl'
+        })
+        .state('newcountriesgroup', {
+            url: '/countriesgroup/new',
+            templateUrl: 'countriesgroup',
+            controller: 'CountriesGroupCtrl'
+        })
+        .state('countriesgroup', {
+            url: '/countriesgroup/:group_id',
+            templateUrl: 'countriesgroup',
+            controller: 'CountriesGroupCtrl'
+        })
 })
 .controller('CountriesCtrl', function($scope, $http) {
     $http.get('/api/country')
@@ -72,5 +87,76 @@ angular.module('dbCountries', [])
             });
     } else {
         $scope.country = {};
+    }
+})
+.controller('CountriesGroupsCtrl', function($scope, $http) {
+    $http.get('/api/countriesgroup')
+        .then(function(response) {
+            $scope.countriesgroups = response.data;
+        });
+})
+.controller('CountriesGroupCtrl', function($scope, $http, $stateParams) {
+    $scope.submit = function () {
+        $http.post('/api/countriesgroup', $scope.group)
+            .then(function(response) {
+                var is_new = !$scope.group.id;
+                $scope.group = response.data;
+                $scope.form.$setPristine();
+                if (is_new) {
+                    $state.go('countriesgroup', {group_id: response.data.id}, {notify:false});
+                }
+            });
+    }
+    $scope.delete = function() {
+        if ($scope.group.id) {
+            $http.delete('/api/countriesgroup/'+$scope.country.id)
+                .then(function(response) {
+                    $state.go('countriesgroups');
+                });
+        } else {
+            $state.go('countriesgroups');
+        }
+    }
+    $http.get('/api/country')
+        .then(function(response) {
+            $scope.countries = response.data;
+        });
+    $scope.name_from_code = function(code) {
+        if ($scope.countries) {
+            for (i=0; i<$scope.countries.length; i++) {
+                if ($scope.countries[i].code == code) {
+                    return $scope.countries[i].name;
+                }
+            }
+        }
+        return code;
+    }
+    $scope.filtered_countries = function() {
+        filtered = [];
+        if ($scope.countries && $scope.group) {
+            for (i=0; i<$scope.countries.length; i++) {
+                if ($scope.group.countries.indexOf($scope.countries[i].code) == -1) {
+                    filtered.push($scope.countries[i])
+                }
+            }
+        }
+        return filtered;
+    }
+    $scope.add = function(code) {
+        $scope.group.countries.push(code);
+        $scope.group.countries.sort();
+        $scope.form.$setDirty();
+    }
+    $scope.remove = function(code) {
+        $scope.group.countries.splice($scope.group.countries.indexOf(code), 1);
+        $scope.form.$setDirty();
+    }
+    if ($stateParams.group_id) {
+        $http.get('/api/countriesgroup/'+$stateParams.group_id)
+            .then(function(response) {
+                $scope.group = response.data;
+            });
+    } else {
+        $scope.group = {};
     }
 })
