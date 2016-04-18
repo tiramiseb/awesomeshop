470c719bb238559ba4ec961e59c3edab759e8d86
Sébastien Maccagnoni-Munch <sebastien@maccagnoni.eu>
2016-04-18 12:01:08 +0200

    Move search index initialization in the app code...
    ... and waaay better search engine, with fuzzy searching, matches with
    and without accentuated characters, etc.

--- awesomeshop/search.py
+++ awesomeshop/search.py
@@ -20,7 +20,11 @@
 import os.path
 
 from whoosh import qparser
-from whoosh.index import open_dir
+from whoosh.analysis import CharsetFilter, SimpleAnalyzer
+from whoosh.fields import Schema, ID, KEYWORD, TEXT
+from whoosh.index import create_in, exists_in, open_dir
+from whoosh.query import FuzzyTerm
+from whoosh.support.charset import accent_map
 
 from . import app
 from .page.api import PageSchemaForList
@@ -30,61 +34,177 @@ from .shop.api.product import ProductSchemaForList
 from .shop.models.category import Category
 from .shop.models.product import Product
 
-doc_index = open_dir(app.config['SEARCH_INDEX_PATH'], indexname='doc')
-category_index = open_dir(app.config['SEARCH_INDEX_PATH'],
-                          indexname='category')
-product_index = open_dir(app.config['SEARCH_INDEX_PATH'], indexname='product')
 
-doc_parser = qparser.MultifieldParser(['title', 'text'],
-                                      schema=doc_index.schema)
-category_parser = qparser.QueryParser('name', schema=category_index.schema)
-product_parser = qparser.MultifieldParser(['name', 'description',
-                                           'reference', 'keywords'],
-                                          schema=product_index.schema)
+indexes = {}
+parsers = {}
+domotego_analyzer = SimpleAnalyzer() | CharsetFilter(accent_map)
+
+class FuzzierTerm(FuzzyTerm):
+    def __init__(self, fieldname, text, boost=1.0, maxdist=3, prefixlength=1, constantscore=True):
+        super(FuzzierTerm, self).__init__(fieldname, text, boost, maxdist, prefixlength, constantscore)
+
+
+def init_indexes_and_parsers():
+    path = app.config['SEARCH_INDEX_PATH']
+    # Initialize the documentations index
+    name = 'doc'
+    if exists_in(path, indexname=name):
+        indexes['doc'] = open_dir(path, indexname=name)
+    else:
+        try:
+            os.makedirs(path)
+        except OSError:
+            pass
+        schema = Schema(
+                        id=ID(stored=True, unique=True),
+                        )
+        schema.add('title_*', TEXT(field_boost=2.0, analyzer=domotego_analyzer), glob=True)
+        schema.add('text_*', TEXT(analyzer=domotego_analyzer), glob=True)
+        indexes['doc'] = create_in(path, schema, indexname=name)
+        index_docs(Page.objects(pagetype='doc'))
+    # Initialize the categories index
+    name = 'category'
+    if exists_in(path, indexname=name):
+        indexes['category'] = open_dir(path, indexname=name)
+    else:
+        try:
+            os.makedirs(path)
+        except OSError:
+            pass
+        schema = Schema(
+                        id=ID(stored=True, unique=True),
+                        )
+        schema.add('name_*', TEXT(field_boost=2.0, analyzer=domotego_analyzer), glob=True)
+        schema.add('description_*', TEXT(analyzer=domotego_analyzer), glob=True)
+        indexes['category'] = create_in(path, schema, indexname=name)
+        index_categories(Category.objects)
+    # Initialize the products index
+    name = 'product'
+    if exists_in(path, indexname=name):
+        indexes['product'] = open_dir(path, indexname=name)
+    else:
+        try:
+            os.makedirs(path)
+        except OSError:
+            pass
+        schema = Schema(
+                        id=ID(stored=True, unique=True),
+                        reference=KEYWORD,
+                        keywords=KEYWORD(lowercase=True, field_boost=1.5)
+                        )
+        schema.add('name_*', TEXT(field_boost=2.0, analyzer=domotego_analyzer), glob=True)
+        schema.add('description_*', TEXT(analyzer=domotego_analyzer), glob=True)
+        indexes['product'] = create_in(path, schema, indexname=name)
+        index_products(Product.objects)
+
+    # Initialize the parsers
+    docparserfields = []
+    categoryparserfields = []
+    productparserfields = ['reference', 'keywords']
+    for lg in app.config['LANGS']:
+        docparserfields.append('title_'+lg)
+        docparserfields.append('text_'+lg)
+        categoryparserfields.append('name_'+lg)
+        categoryparserfields.append('description_'+lg)
+        productparserfields.append('name_'+lg)
+        productparserfields.append('description_'+lg)
+    parsers['doc'] = qparser.MultifieldParser(docparserfields,
+                                      schema=indexes['doc'].schema, termclass=FuzzierTerm)
+    parsers['category'] = qparser.MultifieldParser(categoryparserfields,
+                                      schema=indexes['category'].schema, termclass=FuzzierTerm)
+    parsers['product'] = qparser.MultifieldParser(productparserfields,
+                                              schema=indexes['product'].schema, termclass=FuzzierTerm)
+
+
+def index_docs(objs):
+    writer = indexes['doc'].writer()
+    langs = app.config['LANGS']
+    for i in objs:
+        obj = {
+                'id': unicode(i.id)
+                }
+        for lg in langs:
+            obj['title_'+lg] = i.title.get(lg, u'')
+            obj['text_'+lg] = i.text.get(lg, u'')
+        writer.update_document(**obj)
+    writer.commit()
+
+
+def index_categories(objs):
+    writer = indexes['category'].writer()
+    langs = app.config['LANGS']
+    for i in objs:
+        obj = {
+                'id': unicode(i.id)
+                }
+        for lg in langs:
+            obj['name_'+lg] = i.name.get(lg, u'')
+            obj['description_'+lg] = i.description.get(lg, u'')
+        writer.update_document(**obj)
+    writer.commit()
+
+
+def index_products(objs):
+    writer = indexes['product'].writer()
+    langs = app.config['LANGS']
+    for i in objs:
+        obj = {
+                'id': unicode(i.id),
+                'reference': i.reference,
+                'keywords': i.keywords
+                }
+        for lg in langs:
+            obj['name_'+lg] = i.name.get(lg, u'')
+            obj['description_'+lg] = i.description.get(lg, u'')
+        writer.update_document(**obj)
+    writer.commit()
+
+init_indexes_and_parsers()
 
 
 def do_search(terms):
-    with doc_index.searcher() as searcher:
+    print parsers['doc'].parse(terms)
+    with indexes['doc'].searcher() as searcher:
         docs = []
         for hit in searcher.search(
-                                doc_parser.parse(terms),
-                                collapse='doc',
+                                parsers['doc'].parse(terms),
+                                collapse='id',
                                 limit=None
                                 ):
             try:
                 docs.append(
                         PageSchemaForList().dump(
-                            Page.objects.get(id=hit['doc'], pagetype='doc')
+                            Page.objects.get(id=hit['id'], pagetype='doc')
                             ).data
                         )
             except Page.DoesNotExist:
                 pass
-    with category_index.searcher() as searcher:
+    with indexes['category'].searcher() as searcher:
         categories = []
         for hit in searcher.search(
-                                category_parser.parse(terms),
-                                collapse='doc',
+                                parsers['category'].parse(terms),
+                                collapse='id',
                                 limit=None
                                 ):
             try:
                 categories.append(
                         CategorySchemaForFlatList().dump(
-                            Category.objects.get(id=hit['doc'])
+                            Category.objects.get(id=hit['id'])
                             ).data
                         )
             except Category.DoesNotExist:
                 pass
-    with product_index.searcher() as searcher:
+    with indexes['product'].searcher() as searcher:
         products = []
         for hit in searcher.search(
-                                product_parser.parse(terms),
-                                collapse='doc',
+                                parsers['product'].parse(terms),
+                                collapse='id',
                                 limit=None
                                 ):
             try:
                 products.append(
                         ProductSchemaForList().dump(
-                            Product.objects.get(id=hit['doc'], on_sale=True)
+                            Product.objects.get(id=hit['id'], on_sale=True)
                             ).data
                         )
             except Product.DoesNotExist:
