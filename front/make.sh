3a13c5aa351960a6087a16377d3436dc8adebc52
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2016-09-30 08:30:07 +0200

    Remove an artifact from when I tried using haml

--- front/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-find . -name "*.haml" | while read fname
-do
-    if [ "`basename $fname | cut -c 1`" != "_" ]
-    then
-        haml $fname > `echo $fname | sed 's/.haml$/.html/'`
-    fi
-done
