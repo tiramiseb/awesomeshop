1fc316319b715f29e62f86d4080950401ee92d8b
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2016-11-02 00:59:16 +0100

    Fix missing import

--- back/auth/api.py
+++ back/auth/api.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with AwesomeShop. If not, see <http://www.gnu.org/licenses/>.
 
-from flask import abort, request, session
+from flask import abort, redirect, request, session
 from flask_login import current_user, login_user, logout_user
 from flask_restful import Resource
 from marshmallow import Schema, fields, post_dump, post_load
